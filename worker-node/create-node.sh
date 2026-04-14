8fa261ab108879e5d59112b67c88d28a2d5b0576
Frantisek Sklenar <frantisek.sklenar@gmail.com>
2026-04-14 23:23:29 +0200

    terraform

--- worker-node/create-node.sh
+++ worker-node/create-node.sh
@@ -6,22 +6,21 @@ source .secret
 vmdomain="worker-node.linuxadmin.eu"
 tffolder="worker-node"
 
-
 #Terraform
 cd $HOME/IaC/terraform/terraform-hetzner/${tffolder}/
 terraform apply -auto-approve
 sleep 5
 dns_content=$(terraform output "server_ipv4")
 
-# #wait until server start
-# sleep 30
+#wait until server start
+sleep 30
 
 #Update DNS record
 cd $HOME/IaC/ansible/cloud-vps/
 ssh-keygen -f '$HOME/.ssh/known_hosts' -R '$vmdomain'
 ssh-keyscan -H $vmdomain >> ~/.ssh/known_hosts
 cf_api_token=$(kubectl get secret -n cert-manager cloudflare-api-token -o jsonpath='{.data.cloudflare-api-token}')
-ansible-playbook common/dns.yaml -e dns_content=$dns_content -e cf_api_token="$cf_api_token"
+ansible-playbook common/dns.yaml -e "dns_content=${dns_content} cf_api_token=${cf_api_token} target_hosts=cloud-vps-node01"
 
 #wait for DNS record refresh
 echo "Waiting for DNS record refresh..."
@@ -42,11 +41,17 @@ ansible-playbook common/reboot.yaml -e "target_hosts=cloud-vps-node01"
 cd $HOME/IaC/ansible/cloud-vps/common/
 ansible-playbook docker-install.yaml -e "target_hosts=cloud-vps-node01"
 
-#install kubeadm
+#TailScale k8s
+ansible-playbook tailscale-setup.yaml -e "target_hosts=k8smaster tailscale_client_id=${CLIENT_ID} tailscale_client_secret=${CLIENT_SECRET}"
+
+#TailScale worker-node
+ansible-playbook tailscale-setup.yaml -e "target_hosts=cloud-vps-node01 tailscale_client_id=${CLIENT_ID} tailscale_client_secret=${CLIENT_SECRET}"
+
+#install k8s node packages
 cd $HOME/IaC/ansible/salaserver/k8s-vms/
 ansible-playbook 01-vm-initial-setup.yaml -e "target_hosts=cloud-vps-node01"
 ansible-playbook 02-vm-kube-config.yaml -e "target_hosts=cloud-vps-node01"
 
-#ssh tunnel - control plane to vm
-
-#connect to control-plane - add node
+#complete installation
+cd $HOME/IaC/ansible/cloud-vps/vms/worker-node
+ansible-playbook worker-node.yaml -e "target_hosts=cloud-vps-node01"
